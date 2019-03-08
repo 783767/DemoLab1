953f0f774e9a03699d96a2b8b526ce36175b38fb
Massi Mapani <783767@students.wits.ac.za>
2019-03-08 16:42:10 +0200

    Fixed Deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let path = require('path');
 let express = require('express');
 let app = express();
-let mainRouter = require('mainRoutes.js'));
+let mainRouter = require('mainRoutes.js');
 app.use(mainRouter);
 let port = process.env.PORT || 3000;
 app.listen(port);
